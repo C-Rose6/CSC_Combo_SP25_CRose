4746cf5ce0bc31bd8121e739da76c67cb8813285
unknown <mrkt.connor@gmail.com>
2025-01-31 12:22:10 -0700

    update

---
+++ CSC1001-1060/Time.java/src/Time.java
@@ -0,0 +1,20 @@
+
+public class Time {
+
+	public static void main(String[] args) {
+		
+		int hour = 11;
+		int minute = 01; 
+		
+		System.out.println("The time is ");
+		System.out.print(hour);
+		System.out.print(":");
+		System.out.print(minute);
+		System.out.print(".");
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ CSC1001-1060/Variables/src/Variables.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		String line;
+		Scanner input = new Scanner(System.in);
+		
+		System.out.printf("Four thirds = %.3f", 4.0 / 3.0);
+	}}
